2e6b82b8db9ffc308b1f4e3126509ce5f9eb068c
Ashley Maynez <ashh10254@gmail.com>
2022-08-19 04:55:16 -0500

    Initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,34 @@
+﻿// Ashley Maynez
+// 09/22/2019
+// CS 3410
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = RandomMove(-1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private Vector3 RandomMove(float a, float b)
+    {
+        var temp1 = Random.Range(a, b);
+        var temp2 = Random.Range(a, b);
+        var temp3 = Random.Range(a, b);
+        return new Vector3(temp1, temp2, temp3);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿// Ashley Maynez
+// 09/22/2019
+// CS 3410
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+    public Text countText;
+    private int count;
+    public Text winText;
+    public float totalTime = 60.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+        countText.text = totalTime.ToString();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.velocity = movement * speed;
+        
+    }
+
+    void Update()
+    {
+        totalTime -= Time.deltaTime;
+        countText.text = totalTime.ToString();
+        if (totalTime < 0)
+        {
+            winText.text = "GAMEOVER";
+        }
+ 
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            if (count >= 6)
+            {
+                winText.text = "You win!";
+            }
+        }
+    } 
+}
